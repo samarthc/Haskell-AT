004719400d9a1370528b9041b9882954b4c5c6f2
Samarth Chawla <samarth10@gmail.com>
2016-02-26 20:18:12 +0530

    Use words instead of split''
    Use the standard library function words instead of split''.

--- rpolish.hs
+++ rpolish.hs
@@ -1,5 +1,4 @@
-import MyList (split'')
-import Data.List (lookup)
+--import Data.List (lookup)
 
 --funcs :: [(String, (Num a => a -> a -> a))]
 --funcs = [("+",(+)), ("-", (-)), ("*", (*)), ("/", (/)), ("^",(^))]
@@ -8,10 +7,11 @@ eval (x:y:rest) "+" = show (read y + read x) : rest
 eval (x:y:rest) "-" = show (read y - read x) : rest
 eval (x:y:rest) "*" = show (read y * read x) : rest
 eval (x:y:rest) "/" = show (read y / read x) : rest
+eval (x:y:rest) "^" = show (read y ^ read x) : rest
 eval acc next = next : acc
 
 evaluate :: String -> [String]
-evaluate exp = foldl eval [] . split'' (==' ') $ exp
+evaluate exp = foldl eval [] . words $ exp
 
 result :: String -> Int
 result =  read . sing . evaluate
