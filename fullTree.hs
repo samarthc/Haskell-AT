1503579b2d8535829934865731e6e931542ec05b
Samarth Chawla <samarth10@gmail.com>
2016-03-07 17:28:56 +0530

    Add Foldable instance

--- fullTree.hs
+++ fullTree.hs
@@ -1,3 +1,5 @@
+import qualified Data.Foldable as F
+
 data BSTree a = EmptyTree | Node a (BSTree a) (BSTree a) deriving (Eq, Show, Read) --Binary Search Tree
 
 instance Functor BSTree where
@@ -10,3 +12,7 @@ EmptyTree <*> _ = EmptyTree
 _ <*> EmptyTree = EmptyTree
 (Node f l1 r1) <*> (Node x l2 r2) = Node (f x) (l1 <*> l2) (r1 <*> r2)
 -}
+
+instance F.Foldable BSTree where
+foldMap f EmptyTree = mempty
+foldMap f (Node x left right) = foldMap f left `mappend` f x `mappend` foldMap f right
