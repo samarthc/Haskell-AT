c8f43a0f4063e05c813ce7c3734cfc91772338f7
Samarth Chawla <samarth10@gmail.com>
2016-04-08 18:16:39 +0530

    Add type for error handling

---
+++ LispError.hs
@@ -0,0 +1,30 @@
+module LispError where
+
+import LispVal
+import Control.Monad.Error
+import Data.List (unwords)
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+instance Show LispError where
+    show (NumArgs expected found) = "Expected " ++ show expected ++ " args; found values " ++ (show . map unwords) found
+    show (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ " args; found values " ++ (show . map unwords) found
+    show (Parser parseError) = "Parse error at " ++ show parseError
+    show (BadSpecialForm message form) = message ++ ": " show form
+    show (NotFunction message func) = message ++ ": " ++ func
+    show (UnboundVar message varname) = message ++ ": " ++ varname
+
+instance Error LispError where
+    noMsg = Default "An error has occured"
+    strMsg = Default
+
+trapError action = catchError action (return . show)
+
+extractValue :: Either LispError a -> a
+extractValue (Right val) = val
